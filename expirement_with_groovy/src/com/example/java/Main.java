74c9c663ba097e6ac8d77013df9a581fe871c5ed
Dmitry <kropotovd@gmail.com>
2017-12-10 20:55:12 +0300

    I am commiting expirement_with_groovy.

---
+++ expirement_with_groovy/src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        animal giraffe = new animal();
+        giraffe.x=1;
+        giraffe.y=2;
+        System.out.println("Hello, world. "+giraffe.x+"+"+giraffe.y+"="+giraffe.sum());
+    }
+}
---
+++ expirement_with_groovy/src/com/example/java/animal.java
@@ -0,0 +1,9 @@
+package com.example.java;
+
+public class animal {
+    int x, y;
+
+    public int sum() {
+        return x+y;
+    }
+}
